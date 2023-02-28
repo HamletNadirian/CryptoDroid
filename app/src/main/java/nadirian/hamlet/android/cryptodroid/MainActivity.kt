2cfcbeb89b53175677481bedbbe05ef6fb1b51fc
Hamlet Nadirian <hamlet.nadirian@gmail.com>
2023-02-28 17:47:21 +0100

    Done. Cipher words.

--- app/src/main/java/nadirian/hamlet/android/cryptodroid/MainActivity.kt
+++ app/src/main/java/nadirian/hamlet/android/cryptodroid/MainActivity.kt
@@ -9,6 +9,7 @@ import nadirian.hamlet.android.cryptodroid.databinding.ActivityMainBinding
 
 val number: Char = 'a'
 val numberAsInt = number.code
+var arrayListToString = ""
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,7 +23,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         var plainTextEdt = binding.plaintext.text
-
+        var cipherTextEdt = binding.cipherTextEdt.text
         binding.shiftEdt.setText("1")
 
         binding.shiftEdt.setOnEditorActionListener(OnEditorActionListener { v, actionId, event ->
@@ -30,7 +31,8 @@ class MainActivity : AppCompatActivity() {
                 var shiftFromEditeText = binding.shiftEdt.text.toString().toInt()
                 shiftEdtToInt = shiftFromEditeText
                 var encryptionForExample = encryptionCaesar("a", shiftFromEditeText)
-                var arrayListToStringEncryptionForExample = encryptionForExample.toList().joinToString(" ")
+                // var arrayListToStringEncryptionForExample = encryptionForExample.toList().joinToString(" ")
+                var arrayListToStringEncryptionForExample = encryptionForExample.toList().toString()
                 binding.shiftLetterForExample.setText(arrayListToStringEncryptionForExample)
                 true
             } else false
@@ -40,7 +42,8 @@ class MainActivity : AppCompatActivity() {
             shiftEdtStringTemp = shiftEdtToInt.toString()
             binding.shiftEdt.setText(shiftEdtStringTemp)
             var encryptionForExample = encryptionCaesar("a", shiftEdtToInt)
-            var arrayListToStringEncryptionForExample = encryptionForExample.toList().joinToString(" ")
+            var arrayListToStringEncryptionForExample =
+                encryptionForExample.toList().joinToString(" ")
             binding.shiftLetterForExample.setText(arrayListToStringEncryptionForExample)
 
         }
@@ -51,39 +54,64 @@ class MainActivity : AppCompatActivity() {
             binding.shiftEdt.setText(shiftEdtToInt.toString())
 
             var encryptionForExample = encryptionCaesar("a", shiftEdtToInt)
-            var arrayListToStringEncryptionForExample = encryptionForExample.toList().joinToString(" ")
+            var arrayListToStringEncryptionForExample =
+                encryptionForExample.toList().joinToString(" ")
             binding.shiftLetterForExample.setText(arrayListToStringEncryptionForExample)
 
 
         }
+
         binding.plaintext.setOnEditorActionListener { v, actionId, event ->
             if (actionId == EditorInfo.IME_ACTION_DONE) {
-
                 var encryption = encryptionCaesar(plainTextEdt.toString(), shiftEdtToInt)
-                var arrayListToString = encryption.toList().joinToString(" ")
+                arrayListToString = encryption
                 binding.cipherTextEdt.setText(arrayListToString)
                 true
             }
             false
         }
+
+        binding.cipherTextEdt.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                var decryption = decryptionCaesar(cipherTextEdt.toString(), shiftEdtToInt)
+                arrayListToString = decryption
+                binding.plaintext.setText(arrayListToString)
+                true
+            }
+            false
+        }
+
     }
 }
 
-private fun encryptionCaesar(string: String, shift: Int): ArrayList<Char> {
-
+private fun encryptionCaesar(string: String, shift: Int): String {
     val textStr = ArrayList<Char>()
     for (s in string) {
-        if (s != ' ')
+        if (s == ' ')
+            textStr += ' '
+        else
             textStr.add((s - number + shift).mod(26).plus(numberAsInt).toChar())
     }
-    return textStr
+
+    val to_array: Array<Char> = textStr.toTypedArray()
+    var str = ""
+    to_array.forEach { str += it }
+
+    return str
 }
 
-private fun decryptionCaesar(string: String, shift: Int): ArrayList<Char> {
+private fun decryptionCaesar(string: String, shift: Int): String {
     val textStr = ArrayList<Char>()
     for (s in string) {
-        if (s != ' ')
+        if (s == ' ')
+            textStr += ' '
+        else
             textStr.add((s - number - shift + 26).mod(26).plus(numberAsInt).toChar())
     }
-    return textStr
+
+    val to_array: Array<Char> = textStr.toTypedArray()
+    var str = ""
+    to_array.forEach { str += it }
+
+    return str
 }
